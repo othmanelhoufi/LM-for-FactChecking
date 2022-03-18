a9d201ba9d575eee36e7cf89ea0b29e043ad1e74
Othman EL HOUFI <othmanelhoufi@gmail.com>
2022-03-18 03:24:31 +0100

    Adding BERT model using HuggingFace library

--- code/model/bert_model.py
+++ code/model/bert_model.py
@@ -1,74 +1,154 @@
 #!/usr/bin/env python3
-from simpletransformers.classification import ClassificationModel, ClassificationArgs
+import numpy as np
 import pandas as pd
-import preproc_dataset
 from sklearn.model_selection import train_test_split
-import sklearn
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 import torch
+from transformers import TrainingArguments, Trainer
+from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import EarlyStoppingCallback
+from sklearn.metrics import classification_report
 
-import logging
-import warnings
 
-warnings.filterwarnings("ignore")
-logging.basicConfig(level=logging.ERROR)
+import preproc_dataset
 
-# Get the logger for the huggingface/transformers library.
-transformers_logger = logging.getLogger("transformers")
+# disable WANDB
+import os
+os.environ['WANDB_DISABLED'] = 'true'
 
-# Set the logging level to error, meaning display errors and worse, but
-# don't display any `INFO` logs.
-transformers_logger.setLevel(logging.ERROR)
+# specify GPU
+# device = torch.device("cuda")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Transformer model
+MODEL_NAME = 'bert-base-uncased'
+
+# hyperparams
+MAX_SEQ_LEN = 128
+BATCH_SIZE = 50
+LR = 3e-5
+EPOCHS = 10
+
+# FEVER dataset
+fever = preproc_dataset.FeverDataset()
+DATASET = fever.claim_dataset
+DATASET.columns = ["text", "label"]
+# DATASET = DATASET.sample(150)
+
+
+# Create torch dataset
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels=None):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        if self.labels:
+            item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.encodings["input_ids"])
+
 
 # Preparing train data and eval data
-fever = preproc_dataset.FeverDataset()
-df = fever.claim_dataset
-df = df.sample(50)
-# print(df.describe())
-# exit()
-
-train_df, eval_df = train_test_split(df, test_size=0.3)
-train_df.columns = ["text", "labels"]
-eval_df.columns = ["text", "labels"]
-
-# print("TRAIN : \n", train_df['label'].value_counts())
-# print("EVAL : \n", eval_df['label'].value_counts())
-
-# Preparing
-
-# Optional model configuration
-model_args = ClassificationArgs()
-model_args.output_dir = 'outputs/bert_base_uncased'
-model_args.num_train_epochs = 1
-# model_args.train_batch_size = 500
-# model_args.optimizer = 'AdamW'
-# model_args.learning_rate = 1e-3
-# model_args.evaluate_during_training = True
-# model_args.evaluate_during_training_steps = 100
-# model_args.use_early_stopping = True
-# model_args.early_stopping_delta = 0.01
-# model_args.early_stopping_metric = 'eval_loss'
-# model_args.early_stopping_metric_minimize = True
-# model_args.early_stopping_patience = 3
-# model_args.use_cached_eval_features = True
+def split_dataset(dataset=DATASET):
+    # Preprocess data
+    X = list(dataset['text'])
+    y = list(dataset['label'])
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3)
+    return X_train, y_train, X_val, y_val
 
 
-# Create a ClassificationModel
-cuda_available = torch.cuda.is_available()
-print('GPU available : ', cuda_available)
+# Import BERT Model and BERT Tokenizer
+def init_bert_model(model_name=MODEL_NAME):
+    # import BERT-base pretrained model
+    bert = BertForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=3)
 
-model = ClassificationModel(
-    'bert',
-    'bert-base-uncased',
-    num_labels=3,
-    args=model_args,
-    use_cuda=cuda_available
-)
+    # Load the BERT tokenizer
+    tokenizer = BertTokenizer.from_pretrained(model_name)
 
-# Train the model
-model.train_model(train_df=train_df, eval_df=eval_df, acc=sklearn.metrics.accuracy_score)
+    return bert, tokenizer
 
-# Evaluate the model
-result, model_outputs, wrong_predictions = model.eval_model(eval_df)
 
-# Make predictions with the model
-# predictions, raw_outputs = model.predict(["Sam was a Wizard"])
+
+# Tokenization
+def init_tokens(X_train, X_val, max_seq_len=MAX_SEQ_LEN):
+    # tokenize and encode sequences in the training set
+    tokens_train = tokenizer(X_train, padding=True, truncation=True, max_length=max_seq_len)
+
+    # tokenize and encode sequences in the validation set
+    tokens_val = tokenizer(X_val, padding=True, truncation=True, max_length=max_seq_len)
+
+    return tokens_train, tokens_val
+
+
+# Define Trainer parameters
+def compute_metrics(p):
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
+
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall_micro = recall_score(y_true=labels, y_pred=pred, average='micro')
+    recall_macro = recall_score(y_true=labels, y_pred=pred, average='macro')
+    recall_weighted = recall_score(y_true=labels, y_pred=pred, average='weighted')
+    precision_micro = precision_score(y_true=labels, y_pred=pred, average='micro')
+    precision_macro = precision_score(y_true=labels, y_pred=pred, average='macro')
+    precision_weighted = precision_score(y_true=labels, y_pred=pred, average='weighted')
+    f1_micro = f1_score(y_true=labels, y_pred=pred, average='micro')
+    f1_macro = f1_score(y_true=labels, y_pred=pred, average='macro')
+    f1_weighted = f1_score(y_true=labels, y_pred=pred, average='weighted')
+
+    return {"accuracy": accuracy,
+            "precision_micro": precision_micro, "precision_macro": precision_macro, "precision_weighted": precision_weighted,
+            "recall_micro": recall_micro, "recall_macro": recall_macro, "recall_weighted": recall_weighted,
+            "f1_micro": f1_micro, "f1_macro": f1_macro, "f1_weighted": f1_weighted}
+
+
+if __name__ == '__main__':
+
+    train_text, train_labels, val_text, val_labels = split_dataset(DATASET)
+    bert, tokenizer = init_bert_model(MODEL_NAME)
+    tokens_train, tokens_val = init_tokens(train_text, val_text, MAX_SEQ_LEN)
+
+    train_dataset = Dataset(tokens_train, train_labels)
+    val_dataset = Dataset(tokens_val, val_labels)
+
+
+    # Define Trainer
+    args = TrainingArguments(
+        output_dir="outputs/" + MODEL_NAME,
+        evaluation_strategy="steps",
+        eval_steps=500,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        num_train_epochs=3,
+        seed=0,
+        load_best_model_at_end=True,
+    )
+
+    trainer = Trainer(
+        model=bert,
+        args=args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+    )
+
+    # Train pre-trained model
+    # trainer.train()
+
+    # Load trained model
+    model_path = "outputs/checkpoint-12000"
+    bert = BertForSequenceClassification.from_pretrained(model_path, num_labels=3)
+    # Define test trainer
+    test_trainer = Trainer(bert)
+    # Make prediction
+    raw_pred, _, _ = test_trainer.predict(val_dataset)
+    # Preprocess raw predictions
+    y_pred = np.argmax(raw_pred, axis=1)
+
+    labels = {'REFUTES':0, 'SUPPORTS':1, 'NOT ENOUGH INFO':2}
+    print(classification_report(val_labels, y_pred, target_names=labels))
