3432ad657268a28a16be25a8964ba9f62621d867
Othman EL HOUFI <othmanelhoufi@gmail.com>
2022-05-31 15:46:51 +0200

    cleaning

--- model/tools/tweet_fetcher.py
+++ model/tools/tweet_fetcher.py
@@ -5,11 +5,7 @@ import tweepy
 
 # Twitter Developer keys here
 # It is CENSORED
-api_key = '6BxZqg16tz0ftGwLXc5Cye2Ti'
-api_secret = '1GsAd70iFyHMNnocddXigKeXBsNT2S3yRYgkXTATgmxJ5zTMiZ'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMecdAEAAAAACUiJT32prMz%2F4aP64sUSyUbFeqE%3DLNTI1BTwLjTpTWIPS1gHECFDCxhsxeIBnVZ5os1JOtugLPAIM6'
-access_token = '2211362768-RSmDyTfHpdifbXq4mSboo1N6LgMngjBqT9FWKw8'
-secret_token = 'BKuE2b0awHUbNZI6y0Pr0aVfHTn0emlOlLcL7KFTkebID'
+
 
 #Put your Bearer Token in the parenthesis below
 client = tweepy.Client(bearer_token=bearer_token)
